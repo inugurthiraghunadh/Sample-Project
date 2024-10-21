7e73ca126cbbd14a65c64d4836145ee9c5256efb
Inugurthi Raghunadh <107241095+inugurthiraghunadh@users.noreply.github.com>
2024-10-21 11:03:41 +0530

    DbFirstApproach
    Data base First Approach process

---
+++ DBFirstApproach/Models/BrmContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBFirstApproach.Models;
+
+public partial class BrmContext : DbContext
+{
+    public BrmContext()
+    {
+    }
+
+    public BrmContext(DbContextOptions<BrmContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data source = LAPTOP-HS90P3SF;Database = BRM; TrustServerCertificate=True; Integrated Security=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("products");
+
+            entity.Property(e => e.ProductId).ValueGeneratedNever();
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DBFirstApproach/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirstApproach.Models;
+
+public partial class Product
+{
+    public Guid ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public decimal Price { get; set; }
+}
